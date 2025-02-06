8cddfb1e7f9dd5f95b42414559eb31b1620a36d0
Lucas Sousa <lucassouss97@gmail.com>
2025-02-06 19:57:25 -0300

    created new componentes

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,16 @@ import PrimeiroComponete from './components/PrimeiroComponete.vue';
 import ComDados from './components/ComDados.vue'
 import ComDadosReativosPrimitivos from './components/ComDadosReativosPrimitivos.vue';
 import ComDadosReativosNaoPrimitivos from './components/ComDadosReativosNaoPrimitivos .vue';
+import DataBinding from './components/DataBinding.vue';
+import ComProps from './components/ComProps.vue';
 
 </script>
 
 <template>
   <header>Abobora</header>
   <ComDados/>
+  <ComProps item="Biscoito " :preco="2.50"/>
+  <DataBinding/>
   <ComDadosReativosPrimitivos/>
   <PrimeiroComponete/>
   <ComDadosReativosNaoPrimitivos/>
---
+++ src/components/DataBinding.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="componente">
+        <h1>Componente com Data Binding</h1>
+        <p>{{ valor }}</p>
+        <!-- <input type="text" v-bind:value="valor"> -->
+         <!-- <input type="text" v-bind:value="valor" v-on:input="(e) => valor = e.target.value"> -->
+         <input type="text" v-model="valor"> <!--Elimina a necessidade de usar v-bind e v-on-->
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const valor = ref('Vue é muito show')
+
+</script>
+
+<style>
+
+</style>
